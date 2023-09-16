7c7cb08e0f2bcf026cb5a78eddb9fe3e10e70ccd
Ahmetcan Aksu <ahmetcanaksu@behemehal.org>
2023-09-16 16:37:46 +0300

    Add front

---
+++ frontend/src/layouts/default/AppBar.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app-bar flat>
+    <v-app-bar-title d>
+      <v-icon icon="mdi-information" />
+      Market App
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
